614bf2cc82f455b199d035670506adda39840c7b
rehan atthaillah <traiyhandiscorderaih@gmail.com>
2024-05-23 15:44:39 +0700

    Making SRC (App & Screen) & ASSETS

---
+++ src/screen/one.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
+
+const One = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default One;
