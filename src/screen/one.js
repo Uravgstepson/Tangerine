4bc7c244250d956fa721ece98eea9da07f8dbd27
rehan atthaillah <traiyhandiscorderaih@gmail.com>
2024-05-31 19:27:01 +0700

    New Problem

--- src/screen/one.js
+++ src/screen/one.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {View, Text, ScrollView, FlatList} from 'react-native';
+import {movieData} from '../../assets/Datas/MovieData';
 
 const One = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={{flex: 1}}>
+      <FlatList
+        contentContainerStyle={{padding: 8}}
+        data={movieData}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{
+                margin: 8,
+                backgroundColor: 'lavender',
+                borderWidth: 1,
+              }}>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
